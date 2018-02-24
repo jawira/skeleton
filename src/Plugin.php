7c2e6698cc058fbe873d1e367a5d923efc920520
Jawira <jawitugal@gmail.com>
2018-02-24 08:50:34 +0100

    "MVP"

--- src/Plugin.php
+++ src/Plugin.php
@@ -1,10 +1,5 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: jawira
- * Date: 22/02/18
- * Time: 19:04
- */
+declare(strict_types=1);
 
 namespace Jawira\Defaults;
 
@@ -14,9 +9,6 @@ use Composer\Installer\PackageEvent;
 use Composer\Installer\PackageEvents;
 use Composer\IO\IOInterface;
 use Composer\Plugin\PluginInterface;
-use Composer\Plugin\PluginEvents;
-use Composer\Plugin\PreFileDownloadEvent;
-use Composer\Util\Filesystem;
 
 class Plugin implements PluginInterface, EventSubscriberInterface
 {
@@ -25,8 +17,6 @@ class Plugin implements PluginInterface, EventSubscriberInterface
     protected $composer;
     /** @var  \Composer\IO\IOInterface $io */
     protected $io;
-    /** @var  \Composer\Config $config */
-    protected $config;
     /** @var  \Composer\Util\Filesystem $filesystem */
     protected $filesystem;
 
@@ -34,15 +24,22 @@ class Plugin implements PluginInterface, EventSubscriberInterface
     /**
      * @param \Composer\Composer       $composer
      * @param \Composer\IO\IOInterface $io
+     *
+     * @return \Jawira\Defaults\Plugin
      */
-    public function activate(Composer $composer, IOInterface $io)
+    public function activate(Composer $composer, IOInterface $io): self
     {
-        $this->composer   = $composer;
-        $this->io         = $io;
-        $this->config     = $composer->getConfig();
-        $this->filesystem = new Filesystem();
+        $this->composer = $composer;
+        $this->io       = $io;
+
+        return $this;
     }
 
+    /**
+     * Registering plugin to event
+     *
+     * @return array
+     */
     public static function getSubscribedEvents()
     {
         return [
@@ -50,20 +47,110 @@ class Plugin implements PluginInterface, EventSubscriberInterface
         ];
     }
 
+    /**
+     * Checking current execution before copying resource files
+     *
+     * @param \Composer\Installer\PackageEvent $event
+     *
+     * @return \Jawira\Defaults\Plugin
+     * @throws \RuntimeException
+     */
     public function prepareProject(PackageEvent $event)
     {
-        $basePath = $this->filesystem->normalizePath(realpath(realpath(getcwd())));
-        if (!is_file($basePath. '/composer.json')) {
-            echo 'xxxxxxxxxxx' . $basePath . 'xxxxxxxxxxx';
+
+        $basePath    = $this->getBasePath();
+        $packageName = $this->getPackageName($event);
+
+        $event->getOperations();
+
+        $itsMe = $packageName === 'jawira/defaults';
+
+        if ($itsMe && is_dir($basePath)) {
+            $vendorDir = $this->composer->getConfig()->get('vendor-dir');
+            $this->copyResources($vendorDir, $basePath);
         }
 
-        $job = $event->getOperation()->getJobType();
-        $rea = $event->getOperation()->getReason();
+        return $this;
+    }
 
-        echo get_class($rea);
-        print_r([$job, $rea->getRequiredPackage()]);
+    /**
+     * Copy declared resources to Composer project
+     *
+     * @param $vendorDir
+     * @param $basePath
+     *
+     * @return $this
+     */
+    public function copyResources($vendorDir, $basePath)
+    {
+        foreach ($this->pathsProvider() as [$sourceFormat, $targetFormat]) {
+            $source = sprintf($sourceFormat, $vendorDir);
+            $target = sprintf($targetFormat, $basePath);
 
-//        $this->filesystem->copy($readme, $home);
+            if ($this->canICopyTo($target) && copy($source, $target)) {
+                $this->io->write("<info>Writing $target</info>");
+            }
+        }
+
+        return $this;
+    }
+
+    /**
+     * Provides patters for all files to copy
+     *
+     * @return array
+     */
+    public function pathsProvider()
+    {
+        return [
+            [
+                '%s/jawira/defaults/resources/README.md',
+                '%s/README.md',
+            ],
+            [
+                '%s/jawira/defaults/resources/demo.xml',
+                '%s/demo.xml',
+            ],
+        ];
+    }
+
+    /**
+     * Returns true if file doesn't exist or if it's empty
+     *
+     * @param $file
+     *
+     * @return bool
+     */
+    public function canICopyTo($file)
+    {
+        $fileExists = file_exists($file);
+        $emptyFile  = $fileExists ? filesize($file) === 0 : false;
+
+        return (!$fileExists || $emptyFile);
+    }
+
+    /**
+     * Retrieves the name of the package currently being installed
+     *
+     * @param \Composer\Installer\PackageEvent $event
+     *
+     * @return string
+     */
+    protected function getPackageName(PackageEvent $event)
+    {
+        return $event->getOperation()->getReason()->job['packageName'];
+    }
+
+
+    /**
+     * Retuns project's base dir (where composer.json is)
+     *
+     * @return null|string
+     */
+    protected function getBasePath()
+    {
+        $basePath = realpath(realpath(getcwd()));
+        return is_file($basePath . '/composer.json') ? $basePath : null;
     }
 
 }
