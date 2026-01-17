c70b67e81f84f57ef27ec1b940d7967541101f98
Jawira Portugal <dev@tugal.be>
2026-01-17 18:27:48 +0100

    feat: Add custom fixer to update docblock

--- resources/php-cs-fixer/.php-cs-fixer.dist.php
+++ resources/php-cs-fixer/.php-cs-fixer.dist.php
@@ -2,7 +2,7 @@
 
 $rules = [
   '@PSR12' => true,
-  '@PHP83Migration' => true,
+  '@PHP8x5Migration' => true, // Update this according to your PHP version
   '@PhpCsFixer' => true,
   'return_assignment' => false,
   'declare_strict_types' => true,
@@ -11,4 +11,24 @@ $rules = [
   'global_namespace_import' => ['import_classes' => true, 'import_constants' => true, 'import_functions' => true],
 ];
 
-return (new \PhpCsFixer\Config())->setRules($rules)->setHideProgress(true)->setIndent('  ');
+$headerRules = [
+  'KonradMichalik/docblock_header_comment' => [
+    'annotations' => [
+      'author' => 'Jawira Portugal <dev@tugal.be>',
+      'copyright' => "2026 Jawira Portugal",
+      'license' => 'MIT',
+    ],
+    'preserve_existing' => false,
+    'separate' => 'none',
+    'add_structure_name' => true,
+  ],
+];
+
+return (new \PhpCsFixer\Config())
+  ->setRules($rules)
+  ->setHideProgress(true)
+  ->setIndent('  ')
+  ->registerCustomFixers([
+    new \KonradMichalik\PhpDocBlockHeaderFixer\Rules\DocBlockHeaderFixer(),
+  ])
+  ->setRules($rules + $headerRules);
